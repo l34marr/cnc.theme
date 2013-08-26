a717f244fa4237ddecac4b8f05450bc81796482e
TsungWei Hu <marr.tw@gmail.com>
2013-08-26 16:56:24 +0800

    Add plone.footer Viewlet for PortalFooter Manager.

--- cnc/theme/browser/viewlets.py
+++ cnc/theme/browser/viewlets.py
@@ -1,6 +1,9 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import PersonalBarViewlet
+from plone.app.layout.viewlets.common import PersonalBarViewlet, FooterViewlet
 
 class PersonalBarViewlet(PersonalBarViewlet):
     index = ViewPageTemplateFile('personal_bar.pt')
+
+class FooterViewlet(FooterViewlet):
+    index = ViewPageTemplateFile('footer.pt')
 
